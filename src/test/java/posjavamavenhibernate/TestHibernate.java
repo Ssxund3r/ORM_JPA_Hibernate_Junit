b9a7729e9c3e3b0227d87135c8bd5f0e0bcf83fe
Ssxund3r <gabrielfillip@gmail.com>
2022-04-15 12:41:09 -0300

    Criando a classe Java HibernateUtil e teste com a conexão

---
+++ src/test/java/posjavamavenhibernate/TestHibernate.java
@@ -0,0 +1,13 @@
+package posjavamavenhibernate;
+
+import org.junit.Test;
+
+public class TestHibernate {
+
+	@Test
+	public void testHibernateUtil() {
+
+		HibernateUtil.getEntityManager();
+	}
+
+}
