02fdae32b1010084a2bd6104412cc4ee62c12dd3
Ssxund3r <gabrielfillip@gmail.com>
2022-04-15 17:06:11 -0300

    Criando DAO Genérico

--- src/test/java/posjavamavenhibernate/TestHibernate.java
+++ src/test/java/posjavamavenhibernate/TestHibernate.java
@@ -2,12 +2,26 @@ package posjavamavenhibernate;
 
 import org.junit.Test;
 
+import dao.GenericDao;
+import model.UsuarioPessoa;
+
 public class TestHibernate {
 
 	@Test
 	public void testHibernateUtil() {
 
-		HibernateUtil.getEntityManager();
+		GenericDao<UsuarioPessoa> genericDao = new GenericDao<UsuarioPessoa>();
+		
+		UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
+		usuarioPessoa.setIdade(99);
+		usuarioPessoa.setNome("Fausto");
+		usuarioPessoa.setSobrenome("Silva");
+		usuarioPessoa.setEmail("teste.t@gmail.com");
+		usuarioPessoa.setLogin("Bibo");
+		usuarioPessoa.setSenha("1111");
+		
+		genericDao.salvar(usuarioPessoa);
+		
 	}
 
 }
