aca62acfa7950f28cae16ad325f208c054ae06dc
Ssxund3r <gabrielfillip@gmail.com>
2022-04-15 23:48:53 -0300

    Criando relacionamento entre objetos - OneToMany

--- src/main/java/model/UsuarioPessoa.java
+++ src/main/java/model/UsuarioPessoa.java
@@ -1,18 +1,20 @@
 package model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 
 @Entity
 @NamedQueries({
-	
-	@NamedQuery(name = "UsuarioPessoa.todos", query = "select u from UsuarioPessoa u"),
-	@NamedQuery(name = "UsuarioPessoa.BuscaPorNome", query = "select u from UsuarioPessoa u where u .nome = :nome")
-})
+
+		@NamedQuery(name = "UsuarioPessoa.todos", query = "select u from UsuarioPessoa u"),
+		@NamedQuery(name = "UsuarioPessoa.BuscaPorNome", query = "select u from UsuarioPessoa u where u .nome = :nome") })
 public class UsuarioPessoa {
 
 	@Id
@@ -25,6 +27,17 @@ public class UsuarioPessoa {
 	private String login;
 	private String senha;
 	private int idade;
+
+	@OneToMany(mappedBy = "usuarioPessoa")
+	private List<TelefoneUser> telefoneUsers;
+
+	public void setTelefoneUsers(List<TelefoneUser> telefoneUsers) {
+		this.telefoneUsers = telefoneUsers;
+	}
+
+	public List<TelefoneUser> getTelefoneUsers() {
+		return telefoneUsers;
+	}
 
 	public Long getId() {
 		return id;
